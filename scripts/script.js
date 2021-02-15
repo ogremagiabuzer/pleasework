d6f9b0a57c04e59b46e8fd683c0879a44265c211
ogremagiabuzer <79085545+ogremagiabuzer@users.noreply.github.com>
2021-02-15 20:08:29 +0700

    Fixed the code.
    It didn't work properly.

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,14 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setResponse() {
-  var myResponse = prompt('Please enter your name.');
+  var myResponse = prompt('Please enter your review.');
   localStorage.setItem('response', myResponse);
 }
 
 if(!localStorage.getItem('response')) {
+  setResponse();
+}
+
+myButton.onclick = function() {
   setResponse();
 }
