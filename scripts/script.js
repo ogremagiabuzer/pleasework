8258ed53f3626738d366f12cd0f29685d1852020
ogremagiabuzer <79085545+ogremagiabuzer@users.noreply.github.com>
2021-02-16 15:09:08 +0700

    Added some commentaries for script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,20 @@
+// few variables
 let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
+
+// function for storing the anwser of the prompt in local storage
 
 function setResponse() {
   var myResponse = prompt('Please enter your review.');
   localStorage.setItem('response', myResponse);
 }
 
+// function for calling a setResponse function in order to ask the user for the input if there is no input yet.
+
 if(!localStorage.getItem('response')) {
   setResponse();
 }
+
+// function for changing the review by clicking the button.
 
 myButton.onclick = function() {
   setResponse();
